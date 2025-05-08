8daf0a17aedf69e93e2a490d17888c677fb4af4a
IT Alfa Scorpii <chrisdiantodoni@gmail.com>
2025-05-08 14:53:50 +0700

    update

--- src/components/skills-section.tsx
+++ src/components/skills-section.tsx
@@ -163,7 +163,7 @@ export function SkillsCarousel() {
             drag="x"
             dragConstraints={{ left: 0, right: 0 }}
             dragElastic={1}
-            onDragEnd={(e, { offset, velocity }) => {
+            onDragEnd={(_e, { offset, velocity }) => {
               const swipe = swipePower(offset.x, velocity.x);
 
               if (swipe < -swipeConfidenceThreshold) {
